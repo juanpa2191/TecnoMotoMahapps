dd17fe927edf7dda31486eff8636ac1f2781197a
juan pablo ospina <juanpa2191@hotmail.com>
2017-10-06 17:04:03 -0500

    detalle de compra

---
+++ TecnoMoto/TecnoMoto/ViewModels/DetailBuyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecnoMoto.ViewModels
+{
+    public class DetailBuyViewModel
+    {
+        public DetailBuyViewModel(long? idBuy)
+        {
+
+        }
+    }
+}
