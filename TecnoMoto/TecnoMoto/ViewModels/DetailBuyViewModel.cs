306c8465b815e368430a10765f3df2ba72e0058e
juan pablo ospina <juanpa2191@hotmail.com>
2017-10-20 17:08:07 -0500

    DetailBuy

--- TecnoMoto/TecnoMoto/ViewModels/DetailBuyViewModel.cs
+++ TecnoMoto/TecnoMoto/ViewModels/DetailBuyViewModel.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TecnoMoto.Models;
+using TecnoMoto.Services;
 
 namespace TecnoMoto.ViewModels
 {
-    public class DetailBuyViewModel
+    public class DetailBuyViewModel : ObservableObject
     {
+
+        #region Properties
+        public ObservableCollection<product> listProduct { get; set; }
+
+        public ObservableCollection<users> listUser { get; set; }
+
+        private buy _Buy;
+
+        public buy buyModel
+        {
+            get { return _Buy; }
+            set
+            {
+                _Buy = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #endregion
+
         public DetailBuyViewModel(long? idBuy)
         {
 
